addbc17abc44822591d11f2e7bb1e55a90f4ecc8
hovopd2692 <hovopd2692@gmail.com>
2022-05-30 14:27:06 +0400

    .

---
+++ ServicesApp/Extensions/ServiceControllerExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace WindowServices.Extensions
+{
+    public static class ServiceControllerExtension
+    {
+        public static async Task WaitForStatusAsync(this ServiceController controller, ServiceControllerStatus desiredStatus, TimeSpan timeout)
+        {
+            var utcNow = DateTime.UtcNow;
+            controller.Refresh();
+            while (controller.Status != desiredStatus)
+            {
+                if (DateTime.UtcNow - utcNow > timeout)
+                {
+                    throw new System.TimeoutException($"Failed to wait for '{controller.ServiceName}' to change status to '{desiredStatus}'.");
+                }
+                await Task.Delay(250).ConfigureAwait(false);
+                controller.Refresh();
+            }
+        }
+    }
+}
